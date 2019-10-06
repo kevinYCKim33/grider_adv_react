d51c3c3f0d71fcc1b14734a0f2ff789306038e52
Kevin Yongchul Kim <kevikim33@gmail.com>
2019-10-06 13:43:24 -0700

    wrote my first enzyme test on my own

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,6 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+// import ReactDOM from "react-dom";
 import App from "../App";
+import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 
 // App.js would also work as it is under __tests__ but App.test.js just is more transparent...
 
@@ -15,23 +18,37 @@ import App from "../App";
 // CRA installed a dependency called JSDOM
 // JSDOM: js implementation of how browser works...
 it("shows a comment box", () => {
-  const div = document.createElement("div"); // JSDOM library fake div...
+  const wrapped = shallow(<App />);
+  // wrapped version of our App component;
+  // it has add'l functions loaded onto the top...
+  // sort of like connect()() in redux
+  // grider: i like component for var name...but oh well..
+  // https://airbnb.io/enzyme/docs/api/ShallowWrapper/find.html
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+
+  // RUDIMENTARY VERSION PRE ENZYME
+  // ===================
+  // const div = document.createElement("div"); // JSDOM library fake div...
   // document comes from JSDOM not browser...
   // fake div that lies solely inside memory
   // makes react think that it's inside a browser
-
-  ReactDOM.render(<App />, div);
-
+  // ReactDOM.render(<App />, div);
   // Look inside the div
   // and checks to see if the CommentBox is in there
   // console.log(div.innerHTML);
-  expect(div.innerHTML).toContain("Comment Box"); // pretty terrible at this point...
+  // expect(div.innerHTML).toContain("Comment Box"); // pretty terrible at this point...
   // ^^ why this sux...
   // it tests for internal workings of a component outside of the component it is testing...
   // it just needs to be aware that another component EXISTS
-  expect(div).toHaveAnInstanceOf(CommentBox); // code that I wish I had...
-
-  ReactDOM.unmountComponentAtNode(div);
+  // expect(div).toHaveAnInstanceOf(CommentBox); // code that I wish I had...
+  // npm install --save enzyme enzyme-adapter-react-16 or whatever version you have (16, 17, etc...)
+  // ReactDOM.unmountComponentAtNode(div);
   // basically an undo...
   // formal term: cleanup
+});
+
+// grider exercise
+it("shows a comment list", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,23 @@ import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// Enzyme API
+// 1 Static
+/*
+take component, render it...render html...return object that just returns the html
+can't really interact with the html..
+can't click it, type into it etc...
+*/
+
+// 2 Shallow
+/*
+takes component and renders just that but none of its children...
+test one component by itself...but not any assertion underneath...
+*/
+
+// 3 Full DOM
+/*
+render the component and all of its children + let us modify it afterwards
+can simulate click events
+*/
