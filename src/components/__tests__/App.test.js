df23fada22d01361ae42078d5e66afff5ec3b021
Kevin Yongchul Kim <kevikim33@gmail.com>
2019-10-06 12:52:55 -0700

    finish 17.verfiying component existence

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -7,8 +7,27 @@ import App from "../App";
 // it is same as test
 // grider likes 'it'
 // it is the component we're testing
+
+// jest runs from the terminal!
+// react always expects that it's running inside a browser
+// only functions correctly inside a browser
+
+// CRA installed a dependency called JSDOM
+// JSDOM: js implementation of how browser works...
 it("shows a comment box", () => {
-  const div = document.create("div");
+  const div = document.createElement("div"); // JSDOM library fake div...
+  // document comes from JSDOM not browser...
+  // fake div that lies solely inside memory
+  // makes react think that it's inside a browser
 
   ReactDOM.render(<App />, div);
+
+  // Look inside the div
+  // and checks to see if the CommentBox is in there
+  // console.log(div.innerHTML);
+  expect(div.innerHTML).toContain("Comment Box");
+
+  ReactDOM.unmountComponentAtNode(div);
+  // basically an undo...
+  // formal term: cleanup
 });
